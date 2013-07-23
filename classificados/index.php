23348806d56d451d912890e7bb91935cba9c82a6
UelioNobre <uelionobre@hotmail.com>
2013-07-23 20:47:58 -0300

    Envio do arquivo index com inclusão basica

---
+++ classificados/index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'includes/includes.php';
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
