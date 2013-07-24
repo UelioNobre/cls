9235ec2b1f4ccf47ef919a2067c3606ee4e184f7
UelioNobre <uelionobre@hotmail.com>
2013-07-23 22:38:41 -0300

    Adição da constante raiz do sistema

--- classificados/includes/Config.php
+++ classificados/includes/Config.php
@@ -3,6 +3,10 @@
 // Classe de configuração
 class Config {
 
+    /**
+     * Define a url padrão do sistema
+     */
+    const BASE =   'http://localhost/classificados/';
     const COLUNAS = 4; // Número de colunas de produtos a exibir
 
 }
